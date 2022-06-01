a553f40df0213bd65111d8d1ef32361d31778302
svtyv <106443476+svtyv@users.noreply.github.com>
2022-06-01 09:40:48 +0300

    Add files via upload

---
+++ EP4.py
@@ -0,0 +1,19 @@
+def palindrome():
+    palindromes = []
+    a = 999
+    b = 999
+    while a > 99:
+        while b > 99:
+            ab = int(a * b)
+            ab = str(ab)
+            if ab == ab[::-1]:
+                ab = int(ab)
+                palindromes.append(ab)
+            b -= 1
+        b = 999
+        a -= 1
+    palindromes.sort(key=None, reverse=True)
+    print(palindromes)
+
+
+palindrome()
