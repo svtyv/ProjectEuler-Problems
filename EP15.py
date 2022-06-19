cd504d07e6c4a78a2420e9661d33e4e18c6325e8
svtyv <106443476+svtyv@users.noreply.github.com>
2022-06-19 18:49:19 +0300

    Add files via upload

---
+++ EP15.py
@@ -0,0 +1,29 @@
+def lattice():
+    small_set = []
+    another_set = []
+    highest_value = 0
+    n = 1
+    a = 0
+    k = 1
+    while n < 22:
+        small_set.append(n)
+        n += 1
+    while k <= 10:
+        for i in small_set:
+            a = a + i
+            another_set.append(a)
+        a = another_set[0]
+        small_set.clear()
+        small_set.append(1)
+        for b in another_set[1:]:
+            a = b + a
+            small_set.append(a)
+        highest_value = max(another_set)
+        another_set = []
+        a = 0
+        k +=1
+
+
+    print(small_set)
+    print(highest_value)
+lattice()
